ac35d658ef50e8fd1fe9e0cf01e1d2bd1c43ddf5
yzia2000 <e0381439@u.nus.edu>
2021-04-17 18:16:18 +0800

    reverted fonts

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Cascadia Code PL:pixelsize=12:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=12:antialias=true:autohint=true";
 static char *font2 = "JoyPixels:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
